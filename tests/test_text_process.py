ae53456e623f6837d02893625892d20db2bd919c
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-25 15:44:18 -0400

    added model loading

--- tests/test_text_process.py
+++ tests/test_text_process.py
@@ -17,10 +17,15 @@ class TestTextProcessor(unittest.TestCase):
 
         with tempfile.TemporaryDirectory() as tmpdirname:
             processor = TextProcessor()
-            processor.train_tokenizer(paths, vocab_size=1000, to_save_dir=tmpdirname, model_name="test_model")
+            processor.train_tokenizer(paths, vocab_size=1000, to_save_dir=tmpdirname)
             assert processor.tokenizer.get_vocab_size() == 1000
-            sen = "Obama signed many landmark bills into law during his first two years in office."
-            assert processor.tokenize(sen) is not None
+            sen1 = "Obama signed many landmark bills into law during his first two years in office."
+            assert processor.tokenize(sen1) is not None
+
+            new_prcoessor = TextProcessor(tok_model_path=tmpdirname)
+            assert new_prcoessor.tokenizer.get_vocab_size() == 1000
+            sen2 = "Obama signed many landmark bills into law during his first two years in office."
+            assert processor.tokenize(sen2) is not None
 
 
 if __name__ == '__main__':
--- textprocessor.py
+++ textprocessor.py
@@ -6,10 +6,19 @@ from tokenizers.processors import BertProcessing
 
 
 class TextProcessor:
-    def __init__(self):
-        self.tokenizer = ByteLevelBPETokenizer()
+    def __init__(self, tok_model_path: str = None):
+        if tok_model_path is not None:
+            self.tokenizer = ByteLevelBPETokenizer(
+                tok_model_path + "/vocab.json",
+                tok_model_path + "/merges.txt",
+            )
+            self.tokenizer._tokenizer.post_processor = BertProcessing(
+                ("</s>", self.tokenizer.token_to_id("</s>")),
+                ("<s>", self.tokenizer.token_to_id("<s>")),
+            )
+            self.tokenizer.enable_truncation(max_length=512)
 
-    def train_tokenizer(self, paths: List[str], vocab_size: int, to_save_dir: str, model_name: str):
+    def train_tokenizer(self, paths: List[str], vocab_size: int, to_save_dir: str):
         self.tokenizer = ByteLevelBPETokenizer()
         self.tokenizer.train(files=paths, vocab_size=vocab_size, min_frequency=2, special_tokens=[
             "<s>",
@@ -18,12 +27,12 @@ class TextProcessor:
             "<unk>",
             "<mask>",
         ])
-        self.tokenizer.save(directory=to_save_dir, name=model_name)
         self.tokenizer._tokenizer.post_processor = BertProcessing(
             ("</s>", self.tokenizer.token_to_id("</s>")),
             ("<s>", self.tokenizer.token_to_id("<s>")),
         )
         self.tokenizer.enable_truncation(max_length=512)
+        self.tokenizer.save(directory=to_save_dir)
 
     def tokenize(self, line) -> Encoding:
         return self.tokenizer.encode(line)

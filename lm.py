f83b1f27943e797ea5a1ee0fa672c146dd864959
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-30 17:15:53 -0400

    testing print

--- lm.py
+++ lm.py
@@ -55,6 +55,7 @@ class LM(nn.Module):
         :param data: A minibatch as dictionary that has transformed image and tokenized text as long tensors.
         :return:
         """
+        print("device", device, data["texts"].size())
         texts = data["texts"].to(device)
         pads = data["pad_mask"].to(device)
 

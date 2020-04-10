94f78433944fc93eb794d34fc2fb343d21f6948a
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-04-10 12:29:35 -0400

    do batching same time

--- create_batches.py
+++ create_batches.py
@@ -8,51 +8,88 @@ import torch
 from textprocessor import TextProcessor
 
 
-def write(text_processor: TextProcessor, save_cache_dir: str, max_seq_len: int, txt_file: str,
-          sentence_block_size: int = 10000):
-    if not os.path.exists(save_cache_dir):
-        os.makedirs(save_cache_dir)
-    sentence_block_size = sentence_block_size
+def write(text_processor: TextProcessor, small_cache_dir: str, big_cache_dir: str, max_small_seq_len: int,
+          max_big_seq_len: int, txt_file: str,
+          sentence_small_block_size: int = 40000, sentence_big_block_size: int = 10000):
+    if not os.path.exists(small_cache_dir):
+        os.makedirs(small_cache_dir)
+    sentence_small_block_size = sentence_small_block_size
+    current_small_cache = []
+    small_examples = {}
+    small_line_num, small_file_count = 0, 0
 
-    current_cache = []
-    examples = {}
-    line_num, file_count = 0, 0
+    sentence_big_block_size = sentence_big_block_size
+    current_big_cache = []
+    big_examples = {}
+    big_line_num, big_file_count = 0, 0
+
     with open(os.path.join(txt_file, txt_file), "r") as fp:
         for ln, line in enumerate(fp):
             if len(line.strip()) == 0: continue
             tok_line = text_processor.tokenize_one_line(line.strip())
-            tok_lines = text_processor.split_tokenized(tok_line, max_length=max_seq_len)
-            current_cache += tok_lines
+            small_tok_lines = text_processor.split_tokenized(tok_line, max_length=max_small_seq_len)
+            current_small_cache += small_tok_lines
+            big_tok_lines = text_processor.split_tokenized(tok_line, max_length=max_big_seq_len)
+            current_big_cache += big_tok_lines
 
-            if len(current_cache) >= 100000:
-                for tok_line in current_cache:
+            if len(current_small_cache) >= 100000:
+                for tok_line in current_small_cache:
                     # assuming that every list has same length due to correct padding.
-                    examples[line_num] = torch.LongTensor(tok_line)
-                    line_num += 1
-                    if len(examples) >= sentence_block_size:
-                        with open(os.path.join(save_cache_dir, str(file_count) + ".pkl"), "wb") as fw:
-                            pickle.dump(examples, fw)
-                        examples, file_count = {}, file_count + 1
-                current_cache = []
-                print(f"from {ln} actual documents, dumped {line_num} vectors into {file_count} files")
+                    small_examples[small_line_num] = torch.LongTensor(tok_line)
+                    small_line_num += 1
+                    if len(small_examples) >= sentence_small_block_size:
+                        with open(os.path.join(small_cache_dir, str(small_file_count) + ".pkl"), "wb") as fw:
+                            pickle.dump(small_examples, fw)
+                        small_examples, small_file_count = {}, small_file_count + 1
+                current_small_cache = []
+                print(
+                    f"from {ln} actual documents, dumped {small_line_num} small vectors into {small_file_count} files")
+            if len(current_big_cache) >= 100000:
+                for tok_line in current_big_cache:
+                    # assuming that every list has same length due to correct padding.
+                    big_examples[big_line_num] = torch.LongTensor(tok_line)
+                    big_line_num += 1
+                    if len(big_examples) >= sentence_big_block_size:
+                        with open(os.path.join(big_cache_dir, str(big_file_count) + ".pkl"), "wb") as fw:
+                            pickle.dump(big_examples, fw)
+                        big_examples, big_file_count = {}, big_file_count + 1
+                current_big_cache = []
+                print(f"from {ln} actual documents, dumped {big_line_num} big vectors into {big_file_count} files")
 
-    if len(current_cache) > 0:
-        for tok_line in current_cache:
-            examples[line_num] = torch.LongTensor(tok_line)
-            line_num += 1
-            if len(examples) >= sentence_block_size:
-                with open(os.path.join(save_cache_dir, str(file_count) + ".pkl"), "wb") as fw:
-                    pickle.dump(examples, fw)
-                examples, file_count = {}, file_count + 1
-        if len(examples) >= 0:
-            with open(os.path.join(save_cache_dir, str(file_count) + ".pkl"), "wb") as fw:
-                pickle.dump(examples, fw)
-            examples, file_count = {}, file_count + 1
+    if len(current_small_cache) > 0:
+        for tok_line in current_small_cache:
+            small_examples[small_line_num] = torch.LongTensor(tok_line)
+            small_line_num += 1
+            if len(small_examples) >= sentence_small_block_size:
+                with open(os.path.join(small_cache_dir, str(small_file_count) + ".pkl"), "wb") as fw:
+                    pickle.dump(small_examples, fw)
+                small_examples, small_file_count = {}, small_file_count + 1
+        if len(small_examples) >= 0:
+            with open(os.path.join(small_cache_dir, str(small_file_count) + ".pkl"), "wb") as fw:
+                pickle.dump(small_examples, fw)
+            small_examples, small_file_count = {}, small_file_count + 1
 
-        print(f"from {ln} actual documents, dumped {line_num} vectors into {file_count} files")
+        print(f"from {ln} actual documents, dumped {small_line_num} small vectors into {small_file_count} files")
 
-    with open(os.path.join(save_cache_dir, "info.txt"), "w") as fw:
-        fw.write(str(sentence_block_size) + "\t" + str(line_num) + "\t" + str(file_count))
+    if len(current_big_cache) > 0:
+        for tok_line in current_big_cache:
+            big_examples[big_line_num] = torch.LongTensor(tok_line)
+            big_line_num += 1
+            if len(big_examples) >= sentence_big_block_size:
+                with open(os.path.join(big_cache_dir, str(big_file_count) + ".pkl"), "wb") as fw:
+                    pickle.dump(big_examples, fw)
+                big_examples, big_file_count = {}, big_file_count + 1
+        if len(big_examples) >= 0:
+            with open(os.path.join(big_cache_dir, str(big_file_count) + ".pkl"), "wb") as fw:
+                pickle.dump(big_examples, fw)
+            big_examples, big_file_count = {}, big_file_count + 1
+
+        print(f"from {ln} actual documents, dumped {big_line_num} big vectors into {big_file_count} files")
+
+    with open(os.path.join(small_cache_dir, "info.txt"), "w") as fw:
+        fw.write(str(sentence_small_block_size) + "\t" + str(small_line_num) + "\t" + str(small_file_count))
+    with open(os.path.join(big_cache_dir, "info.txt"), "w") as fw:
+        fw.write(str(sentence_big_block_size) + "\t" + str(big_line_num) + "\t" + str(big_file_count))
 
 
 def get_tokenizer(tokenizer_path: Optional[str] = None, train_path: Optional[str] = None,
@@ -90,10 +127,9 @@ if __name__ == "__main__":
     tokenizer = get_tokenizer(tokenizer_path=options.tokenizer_path, train_path=options.data_path,
                               model_path=options.model_path, vocab_size=options.vocab_size)
 
-    print("writing batch of 128")
-    write(text_processor=tokenizer, save_cache_dir=options.small_cache_path, max_seq_len=128,
-          txt_file=options.data_path, sentence_block_size=4 * options.sentence_block)
-    print("writing batch of 512")
-    write(text_processor=tokenizer, save_cache_dir=options.large_cache_path, max_seq_len=512,
-          txt_file=options.data_path, sentence_block_size=options.sentence_block)
+    print("writing batch of 128/512")
+    write(text_processor=tokenizer, small_cache_dir=options.small_cache_path, big_cache_dir=options.large_cache_path,
+          max_small_seq_len=128, max_big_seq_len=512,
+          txt_file=options.data_path, sentence_small_block_size=4 * options.sentence_block,
+          sentence_big_block_size=options.sentence_block)
     print("finished")

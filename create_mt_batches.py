17a676c9151f82c31f2ff356775aa5de05b966ca
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-05-21 17:38:04 -0400

    ...

--- create_mt_batches.py
+++ create_mt_batches.py
@@ -43,8 +43,7 @@ def write(text_processor: TextProcessor, output_file: str, src_txt_file: str, ds
                 examples[line_num] = torch.LongTensor(src_tok_line)
                 lens[line_num] = len(src_tok_line)
                 line_num += 1
-                if line_num % 1000000 == 0:
-                    print(int(line_num / 1000000), "M", "\r", end="")
+                print(line_num, "M", "\r", end="")
 
         print("\nSorting")
         sorted_lens = sorted(lens.items(), key=lambda item: item[1])

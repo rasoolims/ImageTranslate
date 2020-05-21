824cef422a1e2ea2db08907991e3d8815f7e9e6c
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-05-21 17:37:07 -0400

    better printing

--- create_mt_batches.py
+++ create_mt_batches.py
@@ -44,9 +44,9 @@ def write(text_processor: TextProcessor, output_file: str, src_txt_file: str, ds
                 lens[line_num] = len(src_tok_line)
                 line_num += 1
                 if line_num % 1000000 == 0:
-                    print(int(line_num / 1000000), "M")
+                    print(int(line_num / 1000000), "M", "\r", end="")
 
-        print("Sorting")
+        print("\nSorting")
         sorted_lens = sorted(lens.items(), key=lambda item: item[1])
         sorted_examples = []
         print("Sorted examples")

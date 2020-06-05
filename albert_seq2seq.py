Fixed wrong masking

albert_seq2seq.py
@@ -123,18 +123,18 @@ class AlbertSeq2Seq(nn.Module):
 
 
 class MassSeq2Seq(AlbertSeq2Seq):
-    def forward(self, device, src_inputs, tgt_inputs, src_mask, tgt_mask, mask_pad_mask, log_softmax: bool = False):
+    def forward(self, device, src_inputs, tgt_inputs, src_pads, mask_pad_mask, log_softmax: bool = False):
         """
         :param mask_pad_mask: # Since MASS also generates MASK tokens, we do not backpropagate them during training.
         :return:
         """
         "Take in and process masked src and target sequences."
-        encoder_states = self.encode(device, src_inputs, src_mask)[0]
+        encoder_states = self.encode(device, src_inputs, src_pads)[0]
 
         tgt_inputs = tgt_inputs.to(device)
 
-        subseq_mask = future_mask(tgt_mask[:, :-1]).to(device)
-        decoder_output = self.decoder(encoder_states, tgt_inputs[:, :-1], src_mask, subseq_mask)
+        subseq_mask = future_mask(src_pads[:, :-1]).to(device)
+        decoder_output = self.decoder(encoder_states, tgt_inputs[:, :-1], src_pads, subseq_mask)
         diag_outputs = torch.stack([decoder_output[:, d, d, :] for d in range(decoder_output.size(2))], 1)
         diag_outputs_flat = diag_outputs.view(-1, diag_outputs.size(-1))
 
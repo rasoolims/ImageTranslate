88700446ea74c8a9c26b935122dbfeab19e02f40
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-06-10 21:43:14 -0400

    Reset optimizer before finetuning

--- train_mass.py
+++ train_mass.py
@@ -391,6 +391,10 @@ class MassTrainer(MTTrainer):
 
         finetune_epoch = 0
         while options.finetune_step > 0 and step <= options.finetune_step + options.step:
+            if train_epoch > 0:
+                # Resetting optimizer for fine tuning.
+                trainer.optimizer = train_lm.LMTrainer.build_optimizer(mt_model, options.learning_rate, options.weight_decay)
+                trainer.scheduler.last_epoch = 0
             print("finetune epoch", finetune_epoch)
             _ = trainer.fine_tune(data_iter=finetune_loader, lang_directions=lang_directions,
                                   saving_path=options.model_path, step=step, dev_data_iter=mt_dev_loader)

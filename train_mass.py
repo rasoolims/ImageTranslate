ef0da90b9fbde16280b4fb8f263c07f39a6c77bd
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-06-13 23:43:24 -0400

    Map-based replacement for replacement

--- train_mass.py
+++ train_mass.py
@@ -60,22 +60,11 @@ def mask_text(mask_prob, pad_indices, src_text, text_processor: TextProcessor):
 
     assert 0 < mask_prob < 1
     masked_ids = src_text[:, 1:][src_mask[:, 1:]]
-
     mask_idx = src_text[src_mask]
-    replacements = torch.zeros(mask_idx.size(), dtype=torch.long)
-    for i in range(len(replacements)):
-        r = random.random()
-        if r < 0.8:
-            replacements[i] = text_processor.mask_token_id()
-        elif r < 0.9:
-            # Replace with another random word.
-            random_index = random.randint(len(text_processor.special_tokens), text_processor.vocab_size() - 1)
-            replacements[i] = random_index
-        else:
-            # keep the word
-            replacements[i] = mask_idx[i]
-    src_text[src_mask] = replacements
-
+    random_index = lambda  : random.randint(len(text_processor.special_tokens), text_processor.vocab_size() - 1)
+    rand_select = lambda r, c: text_processor.mask_token_id() if r<0.8 else (random_index() if r<0.9 else int(mask_idx[c]))
+    replacements = list(map(lambda i:rand_select(random.random(), i), range(mask_idx.size(0))))
+    src_text[src_mask] = torch.LongTensor(replacements)
     return src_mask, masked_ids, src_text, to_recover, to_recover_pos, mask_idx
 
 

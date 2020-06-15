d035e3e7006eb18cee5c8910282691674f3f9356
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-06-15 11:20:37 -0400

    Remove short sentences in batch building

--- create_mt_batches.py
+++ create_mt_batches.py
@@ -5,7 +5,8 @@ from optparse import OptionParser
 from textprocessor import TextProcessor
 
 
-def write(text_processor: TextProcessor, output_file: str, src_txt_file: str, dst_txt_file: str = None):
+def write(text_processor: TextProcessor, output_file: str, src_txt_file: str, dst_txt_file: str = None, min_len:int=1,
+          max_len:int=175):
     examples = {}
     line_num = 0
 
@@ -18,9 +19,11 @@ def write(text_processor: TextProcessor, output_file: str, src_txt_file: str, ds
                 src_lang = text_processor.languages[text_processor.id2token(src_tok_line[0])]
                 dst_tok_line = text_processor.tokenize_one_sentence(dst_line.strip())
                 dst_lang = text_processor.languages[text_processor.id2token(dst_tok_line[0])]
-                examples[line_num] = (src_tok_line, dst_tok_line, src_lang, dst_lang)
-                lens[line_num] = len(dst_tok_line)
-                line_num += 1
+
+                if min_len <= len(src_tok_line) <= max_len and min_len <= len(dst_tok_line) <= max_len:
+                    examples[line_num] = (src_tok_line, dst_tok_line, src_lang, dst_lang)
+                    lens[line_num] = len(dst_tok_line)
+                    line_num += 1
 
         print("Sorting")
         sorted_lens = sorted(lens.items(), key=lambda item: item[1])
@@ -42,11 +45,12 @@ def write(text_processor: TextProcessor, output_file: str, src_txt_file: str, ds
                 if len(src_line.strip()) == 0: continue
                 src_tok_line = text_processor.tokenize_one_sentence(src_line.strip())
                 src_lang = text_processor.languages[text_processor.id2token(src_tok_line[0])]
-                examples[line_num] = (src_tok_line, src_lang)
-                lens[line_num] = len(src_tok_line)
-                line_num += 1
-                if line_num % 1000 == 0:
-                    print(line_num, "\r", end="")
+                if min_len <= len(src_tok_line) <= max_len:
+                    examples[line_num] = (src_tok_line, src_lang)
+                    lens[line_num] = len(src_tok_line)
+                    line_num += 1
+                    if line_num % 1000 == 0:
+                        print(line_num, "\r", end="")
 
         print("\nSorting")
         sorted_lens = sorted(lens.items(), key=lambda item: item[1])
@@ -77,6 +81,8 @@ def get_options():
     parser.add_option("--dst", dest="dst_data_path", help="Path to the target txt file", metavar="FILE", default=None)
     parser.add_option("--output", dest="output_path", help="Output marshal file ", metavar="FILE", default=None)
     parser.add_option("--tok", dest="tokenizer_path", help="Path to the tokenizer folder", metavar="FILE", default=None)
+    parser.add_option("--max_seq_len", dest="max_seq_len", help="Max sequence length", type="int", default=175)
+    parser.add_option("--min_seq_len", dest="min_seq_len", help="Max sequence length", type="int", default=2)
     (options, args) = parser.parse_args()
     return options
 

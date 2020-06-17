dfe77c6969710cf9c30b60b6e2cf07dbef1a5ba9
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-06-17 12:23:56 -0400

    Create count_wiki_stats.py

---
+++ scripts/count_wiki_stats.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+path = os.path.abspath(sys.argv[1])
+
+docs, sens = 0, 0
+types = set()
+with open(path, "r") as reader:
+    for line in reader:
+        docs += 1
+        sens += len(line.strip().split("</s>"))
+        types |= set(line.strip().split(" "))
+
+print(docs, "docs,", sens, "sens,", len(types), "types")

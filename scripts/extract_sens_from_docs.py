b27376594ba6f28a299e53203618d3580c3b3603
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-06-18 11:06:14 -0400

    Update extract_sens_from_docs.py

--- scripts/extract_sens_from_docs.py
+++ scripts/extract_sens_from_docs.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 
-def extract_doctences(line, min_len):
+def extract_doctences(line, min_len, max_len):
     line = line.strip()
     if len(line) == 0:
         return []
@@ -11,19 +11,20 @@ def extract_doctences(line, min_len):
     doc_split = docs[0].strip().split(" ")
     docs[0] = " ".join(doc_split[1:])
     lang = doc_split[0]
-    len_condition = lambda s: len(s.strip()) > 0 and len(s.strip().split(" ")) > min_len
+    len_condition = lambda s: len(s.strip()) > 0 and max_len >= len(s.strip().split(" ")) >= min_len
     return list(filter(lambda x: x is not None,
                        map(lambda s: " ".join([lang, s.strip(), "</s>"]) if len_condition(s) else None, docs)))
 
 
 path = os.path.abspath(sys.argv[1])
 min_len = int(sys.argv[2])
-output_path = os.path.abspath(sys.argv[3])
+max_len = int(sys.argv[2])
 
+output_path = os.path.abspath(sys.argv[4])
 
 with open(path, "r") as reader, open(output_path, "w") as writer:
     for i, line in enumerate(reader):
-        sens = extract_doctences(line, min_len)
+        sens = extract_doctences(line, min_len, max_len)
         if len(sens) > 0:
             writer.write("\n".join(sens))
             writer.write("\n")

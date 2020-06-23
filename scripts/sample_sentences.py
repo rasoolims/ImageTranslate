d503e14f1bac55f9e420868ddb3f1b85b7b73dff
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-06-23 17:21:17 -0400

    Update sample_sentences.py

--- scripts/sample_sentences.py
+++ scripts/sample_sentences.py
@@ -8,10 +8,12 @@ output_file = os.path.abspath(sys.argv[3])
 
 sentences = []
 with open(input_file, "r") as reader:
-    for line in reader:
+    for i, line in enumerate(reader):
         sentences.append(line.strip())
+        print(i, end="\r")
 
 random.shuffle(sentences)
 
 with open(output_file, "r") as writer:
     writer.write("\n".join(sentences[:line_count]))
+print("\nDone!")

bb61a8641651703d2ffb1f07497c480bfc45ea4b
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-06-26 10:54:09 -0400

    Update train_mt.py

--- train_mt.py
+++ train_mt.py
@@ -45,6 +45,7 @@ class MTTrainer:
         self.rank = rank
         if self.fp16:
             torch.distributed.init_process_group(backend='nccl')
+            distributed.init_process_group(backend="mpi", group_name="main")
             self.device = torch.device('cuda', rank)
         self.model = self.model.to(self.device)
         self.self_translate = self_translate

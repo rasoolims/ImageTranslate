ac77f9da320cf74fbb270e0460b6bea5d863d2f1
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-07-02 12:06:49 -0400

    Map-based binarization

--- binarize_image_caption_data.py
+++ binarize_image_caption_data.py
@@ -1,3 +1,4 @@
+import datetime
 import marshal
 import os
 from optparse import OptionParser
@@ -7,58 +8,52 @@ from torchvision import transforms
 
 from textprocessor import TextProcessor
 
+transform = transforms.Compose([  # [1]
+    transforms.Resize(256),  # [2]
+    transforms.CenterCrop(224),  # [3]
+    transforms.ToTensor(),  # [4]
+    transforms.Normalize(  # [5]
+        mean=[0.485, 0.456, 0.406],  # [6]
+        std=[0.229, 0.224, 0.225]  # [7]
+    )])
+
+
+def is_valid_img(root_img_dir, path):
+    try:
+        with Image.open(os.path.join(root_img_dir, path)) as im:
+            # make sure not to deal with rgba or grayscale images.
+            _ = transform(im.convert("RGB"))
+            im.close()
+        return True
+    except:
+        return False
+
 
 def write(text_processor: TextProcessor, output_file: str, input_file: str, root_img_dir, skip_check: bool = False,
           max_len: int = 256):
-    transform = transforms.Compose([  # [1]
-        transforms.Resize(256),  # [2]
-        transforms.CenterCrop(224),  # [3]
-        transforms.ToTensor(),  # [4]
-        transforms.Normalize(  # [5]
-            mean=[0.485, 0.456, 0.406],  # [6]
-            std=[0.229, 0.224, 0.225]  # [7]
-        )])
-
     skipped_long_sens = 0
     image_path_dict, unique_images = dict(), dict()
     with open(os.path.join(input_file), "rb") as fp:
         captions = marshal.load(fp)
 
-        tok_captions = []
-        tokenized_sentences = list(map(lambda c: text_processor.tokenize_one_sentence(c[1]), captions))
-        for ci, c in enumerate(captions):
-            print(ci, "/", len(captions), "->", len(tok_captions), len(unique_images), end="\r")
-            try:
-                tok_sen = tokenized_sentences[ci]
-                if len(tok_sen) > max_len:
-                    skipped_long_sens += 1
-                    continue
+        print(datetime.datetime.now(), "Tokenizing sentences")
+        tok_sens = list(map(lambda c: text_processor.tokenize_one_sentence(c[1]), captions))
+        print(datetime.datetime.now(), "Checking images")
+        img_pths = list(map(lambda c: c[0], captions))
+        uniq_img_paths = set(img_pths)
+        valid_images = set(
+            filter(lambda x: x != None, map(lambda i: i if is_valid_img(root_img_dir, i) else None, uniq_img_paths)))
+        unique_images = {k: i for k, i in enumerate(valid_images)}
+        path_ids = {i: k for k, i in enumerate(valid_images)}
 
-                path = c[0]
-                if not skip_check and path not in image_path_dict:
-                    with Image.open(os.path.join(root_img_dir, path)) as im:
-                        # make sure not to deal with rgba or grayscale images.
-                        _ = transform(im.convert("RGB"))
-                        im.close()
-                    image_id = len(unique_images)
-                    unique_images[image_id] = path
-                    image_path_dict[path] = image_id
-                if skip_check and path not in image_path_dict:
-                    image_id = len(unique_images)
-                    unique_images[image_id] = path
-                    image_path_dict[path] = image_id
-                elif path in image_path_dict:
-                    image_id = image_path_dict[path]
-                    unique_images[image_id] = path
-
-                tok_captions.append((image_id, tok_sen))
-            except:
-                pass
-
-        print("Skipped long sentences:", skipped_long_sens, "from", len(captions))
+        print(datetime.datetime.now(), "Getting file captions")
+        captid = lambda i, s: (s, path_ids[img_pths[i]]) if len(s) <= max_len and img_pths[i] in valid_images else None
+        tok_captions = list(filter(lambda x: x != None, map(lambda i, s: captid(i, s), range(len(captions)), tok_sens)))
+        print(datetime.datetime.now(), "Dumping...")
         with open(output_file, "wb") as wfp:
             marshal.dump((unique_images, tok_captions), wfp)
-        print("Dumped", len(tok_captions), "captions from", len(unique_images), "unique images")
+        print(datetime.datetime.now(), "Dumped", len(tok_captions), "captions from", len(unique_images),
+              "unique images")
 
 
 def get_options():

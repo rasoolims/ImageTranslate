a4e5ca57adbb7c94afb96800c0de4442ed0f3f10
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-07-08 10:52:25 -0400

    Create eval_sacre_bleu.py

---
+++ scripts/eval_sacre_bleu.py
@@ -0,0 +1,18 @@
+from optparse import OptionParser
+
+import sacrebleu
+
+
+def get_lm_option_parser():
+    parser = OptionParser()
+    parser.add_option("--output", dest="output", metavar="FILE", default=None)
+    parser.add_option("--gold", dest="gold", metavar="FILE", default=None)
+    return parser
+
+
+(options, args) = get_lm_option_parser().parse_args()
+
+output = open(options.output, "r").read().strip().split("\n")
+gold = [open(options.gold, "r").read().strip().split("\n")]
+
+print(sacrebleu.corpus_bleu(output, gold))

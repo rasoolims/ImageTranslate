ccbdb45cfdc522b22c854b251b34e9bfec9faa28
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-07-17 15:52:40 -0400

    Update train_image_doc_mt.py

--- train_image_doc_mt.py
+++ train_image_doc_mt.py
@@ -250,8 +250,7 @@ class ImageDocTrainer:
                 except RuntimeError as err:
                     print(repr(err))
                     print("Error processing", is_img_batch)
-                    if (isinstance(model, ImageCaptionSeq2Seq) or isinstance(model, ImageMassSeq2Seq)) and (
-                            is_img_batch or is_img_caption_mass):
+                    if (isinstance(model, ImageMassSeq2Seq)) and (is_img_batch or is_img_caption_mass):
                         for b in batch:
                             print("->", len(b["images"]), b["captions"].size())
                     torch.cuda.empty_cache()

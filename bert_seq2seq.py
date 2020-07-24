8a1fd02002d7fa588e1866a7a76ac3725bf831fa
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-07-24 16:47:36 -0400

    fix fp16 related issue

--- bert_seq2seq.py
+++ bert_seq2seq.py
@@ -22,6 +22,21 @@ class BertDecoderModel(BertPreTrainedModel):
         self.decoder = BertEncoder(self.config)
         self.init_weights()
 
+    def invert_attention_mask(self, encoder_attention_mask):
+        """type: torch.Tensor -> torch.Tensor"""
+        if encoder_attention_mask.dim() == 3:
+            encoder_extended_attention_mask = encoder_attention_mask[:, None, :, :]
+        if encoder_attention_mask.dim() == 2:
+            encoder_extended_attention_mask = encoder_attention_mask[:, None, None, :]
+        # T5 has a mask that can compare sequence ids, we can simulate this here with this transposition
+        # Cf. https://github.com/tensorflow/mesh/blob/8d2465e9bc93129b913b5ccc6a59aa97abd96ec6/mesh_tensorflow
+        # /transformer/transformer_layers.py#L270
+        # encoder_extended_attention_mask = (encoder_extended_attention_mask ==
+        # encoder_extended_attention_mask.transpose(-1, -2))
+        encoder_extended_attention_mask = encoder_extended_attention_mask.to(dtype=self.dtype)  # fp16 compatibility
+        encoder_extended_attention_mask = (1.0 - encoder_extended_attention_mask) *  -10000.0
+        return encoder_extended_attention_mask
+
     def forward(
             self,
             input_ids=None,

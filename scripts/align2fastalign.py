87dbb541e3cf582610a8085f2594452bb916b6cb
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-07-29 15:56:00 -0400

    Update align2fastalign.py

--- scripts/align2fastalign.py
+++ scripts/align2fastalign.py
@@ -6,8 +6,13 @@ r2 = open(os.path.abspath(sys.argv[2]), 'r')
 w = open(os.path.abspath(sys.argv[3]), 'w')
 
 l1 = r1.readline()
+count = 0
 while l1:
     l2 = r2.readline()
     w.write(l1.strip() + ' ||| ' + l2.strip() + '\n')
     l1 = r1.readline()
+    count+=1
+    if count %10000==0:
+        print(count, end="\r")
 w.close()
+print('\nDone')

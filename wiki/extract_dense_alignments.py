482b0be860415461c70b51afb5db5f1eb5d34228
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-07-30 16:00:05 -0400

    Create extract_dense_alignments.py

---
+++ wiki/extract_dense_alignments.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+input_path = os.path.abspath(sys.argv[1])
+alignment_path = os.path.abspath(sys.argv[2])
+proportion = float(sys.argv[3])
+output_path = os.path.abspath(sys.argv[4])
+
+with open(input_path, "r") as reader, open(alignment_path, "r") as areader, open(output_path, "w") as w:
+    used = 0
+    for i, line, aline in enumerate(zip(reader, areader)):
+        spl = line.strip().split(" ||| ")
+        src_len = len(spl[0].strip().split(" "))
+        dst_len = len(spl[1].strip().split(" "))
+        sen_len = min(src_len, dst_len)
+        alen = len(aline.strip().split(" "))
+        if alen / sen_len >= proportion:
+            w.write(line.strip() + "\n")
+            used += 1
+        print(used, "/", i + 1, end="\r")
+print("\nDone!")

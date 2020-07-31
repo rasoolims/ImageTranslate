7a11ec79d9404084166a1f3aa0eba209ba879b17
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-07-31 17:09:30 -0400

    Update fastalign2align.py

--- scripts/fastalign2align.py
+++ scripts/fastalign2align.py
@@ -8,6 +8,6 @@ output_path2 = os.path.abspath(sys.argv[3])
 with open(input_path, "r") as reader, open(output_path1, "w") as w1, open(output_path2, "w") as w2:
     for line in reader:
         spl = line.strip().split(" ||| ")
-        if len(spl) != 2: continue
+        if len(spl) < 2: continue
         w1.write(spl[0] + "\n")
         w2.write(" ".join(spl[1:]).strip() + "\n")

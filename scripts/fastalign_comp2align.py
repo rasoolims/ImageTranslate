8764dd55d55b7f1fa2e6edb52f3c4e32379b4af7
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-08-05 10:35:52 -0400

    Create fastalign_comp2align.py

---
+++ scripts/fastalign_comp2align.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+input_path = os.path.abspath(sys.argv[1])
+output_path1 = os.path.abspath(sys.argv[2])
+output_path2 = os.path.abspath(sys.argv[3])
+
+with open(input_path, "r") as reader, open(output_path1, "w") as w1, open(output_path2, "w") as w2:
+    for line in reader:
+        spl = line.strip().split(" ||| ")
+        if len(spl) < 2: continue
+        for j in range(1, len(spl)):
+            w1.write(spl[0] + "\n")
+            w2.write(spl[j].strip() + "\n")

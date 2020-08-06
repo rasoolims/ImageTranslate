e4f8f986dfdd3c52080218985329d627d6b8bdc7
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-08-06 15:40:10 -0400

    Update download_from_list.py

--- scripts/web_captions/download_from_list.py
+++ scripts/web_captions/download_from_list.py
@@ -3,19 +3,20 @@ import sys
 
 input_list = open(os.path.abspath(sys.argv[1]), "r").read().strip().split("\n")
 output_folder = os.path.abspath(sys.argv[2])
+begin = int(sys.argv[3])
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 log_file = os.path.join(output_folder, "log.txt")
 
-with open(os.path.join(output_folder, "list.txt"), "w") as writer:
-    for i, url in enumerate(input_list):
-        writer.write(str(i) + "\t" + url + "\n")
-
+for i, url in enumerate(input_list[begin:]):
+    try:
         command = ["wget -k --tries=1 --timeout=5", url, "-O", os.path.join(output_folder, str(i)), "-o", log_file]
-        if (i + 1) % 100 != 0:
+        if (i + begin + 1) % 100 != 0:
             command.append("&")
         else:
-            print((i + 1), "/", len(input_list), end="\r")
+            print((i + begin + 1), "/", len(input_list), end="\r")
 
         os.system(" ".join(command))
+    except:
+        pass
 print("\nDone!")

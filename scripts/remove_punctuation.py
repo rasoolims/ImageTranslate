7044fb69228e85643c21b2e3844aa9beb8ddb1de
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-08-13 11:52:23 -0400

    Create remove_punctuation.py

---
+++ scripts/remove_punctuation.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+path = lambda i: os.path.abspath(sys.argv[i])
+
+# define punctuation
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+with open(path(1), "r") as r, open(path(2), "w") as w:
+    for line in r:
+        no_punct = []
+        for char in line.strip():
+            if char not in punctuations:
+                no_punct.append(char)
+        w.write("".join(no_punct))
+        w.write("\n")

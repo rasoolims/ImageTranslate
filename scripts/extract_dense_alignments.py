393fcadd9eb5a8d37b733b77560ae3d8d7247916
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-08-13 12:42:39 -0400

    Update extract_dense_alignments.py

--- scripts/extract_dense_alignments.py
+++ scripts/extract_dense_alignments.py
@@ -1,6 +1,9 @@
 import os
 import sys
 
+len_condition = lambda words1, words2: True if .9 <= len(words1) / len(words2) <= 1.1 or abs(
+    len(words1) - len(words2)) <= 5 and len(words1) >= 5 and len(words2) >= 5 else False
+
 src_path = os.path.abspath(sys.argv[1])
 dst_path = os.path.abspath(sys.argv[2])
 alignment_path = os.path.abspath(sys.argv[3])
@@ -16,7 +19,8 @@ with open(src_path, "r") as sr, open(dst_path, "r") as dr, open(alignment_path, 
         alignments = alignment.strip().split(" ")
 
         density = len(alignments) / max(len(src_words), len(dst_words))
-        if density >= min_density and len(src_words) >= 5 and len(dst_words) >= 5:
+        if density >= min_density and len(src_words) >= 5 and len(dst_words) >= 5 and len_condition(src_words,
+                                                                                                    dst_words):
             w.write(src.strip() + " ||| " + dst.strip() + "\n")
             written += 1
 

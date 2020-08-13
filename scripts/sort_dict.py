3266281bff669f8c72265851d6fd38356d488475
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-08-13 11:21:31 -0400

    Update sort_dict.py

--- scripts/sort_dict.py
+++ scripts/sort_dict.py
@@ -1,11 +1,13 @@
 import os
 import sys
 
+threshold = float(sys.argv[3])
+
 pair_dict = {}
 with open(os.path.abspath(sys.argv[1]), "r") as r:
     for line in r:
         spl = line.strip().split("\t")
-        if len(spl) == 3:
+        if len(spl) == 3 and float(spl[2]) >= threshold:
             pair_dict[spl[0] + "\t" + spl[1]] = float(spl[2])
 
 pair_dict = sorted(pair_dict.items(), key=lambda x: x[1], reverse=True)

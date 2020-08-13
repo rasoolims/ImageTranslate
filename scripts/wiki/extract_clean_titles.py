404b68011760b39642bdc30f674d345ade312ccb
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-08-13 15:36:28 -0400

    Create extract_clean_titles.py

---
+++ scripts/wiki/extract_clean_titles.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+print("\nReading docs")
+found = 0
+with open(os.path.abspath(sys.argv[1]), "r") as reader, open(os.path.abspath(sys.argv[3]), "w") as writer:
+    for i, line in enumerate(reader):
+        src, dst = line.strip().split("\t")
+        if "(" not in src and "(" not in dst:
+            writer.write(src + " ||| " + dst + "\n")
+            found += 1
+
+        print(found, "/", i, end="\r")
+print("\nDone!")

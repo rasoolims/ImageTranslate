fffda2f98f562dee07499e8bcbfdd66714d7f181
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-08-13 15:38:27 -0400

    Update extract_clean_titles.py

--- scripts/wiki/extract_clean_titles.py
+++ scripts/wiki/extract_clean_titles.py
@@ -5,10 +5,13 @@ print("\nReading docs")
 found = 0
 with open(os.path.abspath(sys.argv[1]), "r") as reader, open(os.path.abspath(sys.argv[2]), "w") as writer:
     for i, line in enumerate(reader):
-        src, dst = line.strip().split("\t")
-        if "(" not in src and "(" not in dst:
-            writer.write(src + " ||| " + dst + "\n")
-            found += 1
+        try:
+            src, dst = line.strip().split("\t")
+            if "(" not in src and "(" not in dst:
+                writer.write(src + " ||| " + dst + "\n")
+                found += 1
+        except:
+            pass
 
         print(found, "/", i, end="\r")
 print("\nDone!")

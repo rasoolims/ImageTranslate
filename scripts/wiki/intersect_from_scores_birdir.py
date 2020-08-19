c212d62d6774f57be8bd5c89a8ce2de1d78bbc65
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-08-19 15:56:41 -0400

    Update intersect_from_scores_birdir.py

--- scripts/wiki/intersect_from_scores_birdir.py
+++ scripts/wiki/intersect_from_scores_birdir.py
@@ -19,7 +19,7 @@ found = 0
 with open(output_path, "w") as w:
     for src in forward_dict.keys():
         dst, p1 = forward_dict[src]
-        if dst in forward_dict and forward_dict[dst] == src:
+        if dst in forward_dict and forward_dict[dst][0] == src:
             _, p2 = forward_dict[dst]
             w.write(src + " ||| " + dst + "\t" + str(p1) + "\t" + str(p2) + "\t" + str(p1 + p2) + "\n")
             found += 1

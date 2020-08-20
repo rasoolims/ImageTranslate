5666d77d96c17d420eea183434b7a8fbbb48c408
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-08-20 11:11:57 -0400

    Update sample_sentences.py

--- scripts/sample_sentences.py
+++ scripts/sample_sentences.py
@@ -4,12 +4,14 @@ import sys
 
 input_file = os.path.abspath(sys.argv[1])
 line_count = int(sys.argv[2])
-output_file = os.path.abspath(sys.argv[3])
+min_len, max_len = int(sys.argv[3]), int(sys.argv[4])
+output_file = os.path.abspath(sys.argv[5])
 
 sentences = []
 with open(input_file, "r") as reader:
     for i, line in enumerate(reader):
-        sentences.append(line.strip())
+        if min_len <= len(line.strip().split(" ")) <= max_len:
+            sentences.append(line.strip())
         print(i, end="\r")
 
 random.shuffle(sentences)

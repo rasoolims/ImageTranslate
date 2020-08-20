5fad6bb0005a9fb3b637bfc4ae302065319c5b63
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-08-20 13:00:52 -0400

    Update sample_sentences.py

--- scripts/sample_sentences.py
+++ scripts/sample_sentences.py
@@ -12,9 +12,10 @@ with open(input_file, "r") as reader:
     for i, line in enumerate(reader):
         if min_len <= len(line.strip().split(" ")) <= max_len:
             sentences.append(line.strip())
-        print(i, end="\r")
+        if i % 1000 == 0:
+            print(len(sentences), "/", i, end="\r")
 
-print(len(sentences))
+print("\n", len(sentences))
 random.shuffle(sentences)
 
 with open(output_file, "w") as writer:

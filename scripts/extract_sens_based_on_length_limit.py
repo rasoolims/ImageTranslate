d724ab3510f02a7c67a2a9eba332be02018f9845
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-08-25 10:24:28 -0400

    Create extract_sens_based_on_length_limit.py

---
+++ scripts/extract_sens_based_on_length_limit.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+min_len = int(sys.argv[2])
+max_len = int(sys.argv[2])
+
+wrote = 0
+with open(os.path.abspath(sys.argv[1]), "r") as r, open(os.path.abspath(sys.argv[4]), "w") as w:
+    for i, s in enumerate(r):
+        s = s.strip()
+        spl = s.split(" ")
+        if min_len <= len(spl) <= max_len:
+            wrote += 1
+            w.write(s + "\n")
+
+        if i % 1000 == 0:
+            print(wrote, "/", i, end="\r")
+
+print("\nDone", wrote)

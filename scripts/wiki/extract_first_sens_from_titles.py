55cefaa7e02ca714113f37a8ddbcb96391439681
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-09-03 16:26:25 -0400

    Using all sentences in range 5

--- scripts/wiki/extract_first_sens_from_titles.py
+++ scripts/wiki/extract_first_sens_from_titles.py
@@ -42,28 +42,33 @@ with open(os.path.abspath(sys.argv[3]), "r") as dst_reader, open(os.path.abspath
     for i, line in enumerate(dst_reader):
         sentences = line.strip().split("</s>")
         title = sentences[0][sentences[0].find(">") + 1:].strip()
-        first_sentence = sentences[1]
 
         if title in title_dict:
             src_title = title_dict[title]
             if src_title in src_docs:
-                src_first_sentence = src_docs[src_title][1]
-                first_sentence = first_sentence.replace("()", "").replace("  ", " ").strip()
-                src_first_sentence = src_first_sentence.replace("()", "").replace("  ", " ").strip()
-                if first_sentence.lower().startswith("early life"):
-                    continue  # Common phrase in Wiki
-                if "list of" in first_sentence.lower():
-                    continue  # Common phrase in Wiki
-                n1 = has_number(first_sentence)
-                n2 = has_number(src_first_sentence)
-                sen_words2 = first_sentence.strip().split(" ")
-                sen_words1 = src_first_sentence.strip().split(" ")
-                if not len_condition(sen_words1, sen_words2):
-                    continue
-                if (n1 and n2) or (not n1 and not n2):
-                    if src_first_sentence.lower() != first_sentence.lower():
-                        first_sen_writer.write(src_first_sentence + " ||| " + first_sentence + "\n")
-                        found += 1
+                src_sens = src_docs[src_title]
+                for i in range(1, min(6, len(sentences))):
+                    for j in range(min(5, len(src_sens))):
+                        first_sentence = sentences[i]
+                        src_first_sentence = src_sens[j]
+
+                        first_sentence = first_sentence.replace("()", "").replace("  ", " ").strip()
+                        src_first_sentence = src_first_sentence.replace("()", "").replace("  ", " ").strip()
+                        if first_sentence.lower().startswith("early life"):
+                            continue  # Common phrase in Wiki
+                        if "list of" in first_sentence.lower():
+                            continue  # Common phrase in Wiki
+                        n1 = has_number(first_sentence)
+                        n2 = has_number(src_first_sentence)
+                        sen_words2 = first_sentence.strip().split(" ")
+                        sen_words1 = src_first_sentence.strip().split(" ")
+                        if not len_condition(sen_words1, sen_words2):
+                            continue
+                        if (n1 and n2) or (not n1 and not n2):
+                            if src_first_sentence.lower() != first_sentence.lower():
+                                first_sen_writer.write(src_first_sentence + " ||| " + first_sentence + "\n")
+                                found += 1
+                                break
 
         print(found, "/", i, end="\r")
 print("\nDone!")

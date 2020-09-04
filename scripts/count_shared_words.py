ebb5da37416d71e8a46b2f3af84e9b241958eb42
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-09-04 11:57:45 -0400

    Update count_shared_words.py

--- scripts/count_shared_words.py
+++ scripts/count_shared_words.py
@@ -2,12 +2,16 @@ import os
 import sys
 
 s1, s2 = set(), set()
+c1, c2 = set(), set()
 with open(os.path.abspath(sys.argv[1]), "r") as r:
     for line in r:
         s1 |= set(line.strip().split(" "))
+        c1 |= set(list(line.strip()))
 
 with open(os.path.abspath(sys.argv[2]), "r") as r:
     for line in r:
         s2 |= set(line.strip().split(" "))
+        c2 |= set(list(line.strip()))
 
 print(len(s1), len(s2), len(s1 & s2))
+print(len(c1), len(c2), len(c1 & c2))

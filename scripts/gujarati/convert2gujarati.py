12fbe04fe39b5b5d1cc4ae2512533b7f9c8b4b23
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-09-04 11:06:28 -0400

    Update convert2gujarati.py

--- scripts/gujarati/convert2gujarati.py
+++ scripts/gujarati/convert2gujarati.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 
 from indic_transliteration import sanscript
@@ -6,6 +7,8 @@ from indic_transliteration.sanscript import transliterate
 
 
 def isEnglish(s):
+    if s.isdigit():
+        return random.random() <= 0.5  # With 50% chance, convert digits to Gujarati.
     try:
         s.encode(encoding='utf-8').decode('ascii')
     except UnicodeDecodeError:
@@ -20,8 +23,9 @@ with open(input_text, "r") as r, open(output_text, "w") as w:
     for i, line in enumerate(r):
         line = line.strip()
         converted = []
-        w.write("".join(
-            map(lambda c: c if isEnglish(c) else transliterate(c, sanscript.ITRANS, sanscript.GUJARATI), list(line))))
+        w.write(" ".join(
+            map(lambda c: c if isEnglish(c) else transliterate(c, sanscript.ITRANS, sanscript.GUJARATI),
+                line.split(" "))))
         w.write("\n")
         if i % 10000 == 0:
             print(i, end="\r")

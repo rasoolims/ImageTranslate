397332594d99b1f35ac97ecf8d7c092bc961137a
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-09-08 14:48:14 -0400

    Uniq dense alignments

--- scripts/wiki/extract_dense_alignments.py
+++ scripts/wiki/extract_dense_alignments.py
@@ -6,6 +6,7 @@ alignment_path = os.path.abspath(sys.argv[2])
 proportion = float(sys.argv[3])
 output_path = os.path.abspath(sys.argv[4])
 
+uniq_best = dict()
 with open(input_path, "r") as reader, open(alignment_path, "r") as areader, open(output_path, "w") as w:
     used = 0
     for i, (line, aline) in enumerate(zip(reader, areader)):
@@ -14,9 +15,15 @@ with open(input_path, "r") as reader, open(alignment_path, "r") as areader, open
         dst_len = len(spl[1].strip().split(" "))
         sen_len = min(src_len, dst_len)
         alen = len(aline.strip().split(" "))
-        if alen / sen_len >= proportion:
-            w.write(line.strip() + "\n")
-            used += 1
+        prop = alen / sen_len
+        if prop >= proportion:
+            if (spl[0] not in uniq_best) or uniq_best[spl[0]][1] < prop:
+                uniq_best[spl[0]] = (spl[1], prop)
+                used += 1
         if i % 1000 == 0:
             print(used, "/", i + 1, end="\r")
+
+    for sen in uniq_best.keys():
+        w.write(sen + " ||| " + uniq_best[sen][0] + "\n")
+
 print("\nDone!")

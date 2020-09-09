84ae5bb68b0c52515eb33e133247fed4ae7bfe2d
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-09-09 13:59:29 -0400

    Update mmiddict2parallel.py

--- scripts/mmiddict2parallel.py
+++ scripts/mmiddict2parallel.py
@@ -5,7 +5,8 @@ ap = lambda x: os.path.abspath(x)
 
 with open(ap(sys.argv[1]), "r") as r, open(ap(sys.argv[2]), "w") as w1, open(ap(sys.argv[3]), "w") as w2:
     for line in r:
-        spl = line.strip().split("\t")
+        spl = [s.strip() for s in line.strip().split("\t")]
         for i in range(1, len(spl)):
-            w1.write(spl[0] + "\n")
-            w2.write(spl[i] + "\n")
+            if len(spl[0]) > 1 and len(spl[1]) > 1:
+                w1.write(spl[0] + "\n")
+                w2.write(spl[i] + "\n")

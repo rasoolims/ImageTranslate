877b72b7261cc21b8b0362aa4147ec0e7cc863b8
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-09-09 13:47:12 -0400

    Create mmiddict2parallel.py

---
+++ scripts/mmiddict2parallel.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+ap = lambda x: os.path.abspath(x)
+
+with open(ap(sys.argv[1]), "r") as r, open(ap(sys.argv[2]), "w") as w1, , open(ap(sys.argv[3]), "w") as w2:
+    for line in r:
+        spl = line.strip().split("\t")
+        for i in range(1, len(spl)):
+            w1.write(spl[0] + "\n")
+            w2.write(spl[i] + "\n")

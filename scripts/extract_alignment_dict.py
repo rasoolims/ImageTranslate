e93af2d28be58ce1dd1c0df52eec62120019f04e
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-09-25 16:20:28 -0400

    Update extract_alignment_dict.py

--- scripts/extract_alignment_dict.py
+++ scripts/extract_alignment_dict.py
@@ -22,6 +22,7 @@ with open(fast_align_path, "r") as dr, open(alignment_path, "r") as ar:
             coocs += alignment_counter(alignments, src_words, dst_words)
         except Exception as err:
             print(repr(err))
+            print(alignment.strip().split(" "))
             print(src)
             print(dst)
         print(i, end="\r")

63d50b6f775017f9e5a798f21855f9c3f96578e9
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-09-29 13:33:16 -0400

    Update extract_parallel_using_dict.py

--- comparable/extract_parallel_using_dict.py
+++ comparable/extract_parallel_using_dict.py
@@ -25,26 +25,27 @@ with open(os.path.abspath(sys.argv[1]), "r") as dict_reader:
 print("Reading corpus")
 src2dst_sen_max_sim = dict()
 dst2src_sen_max_sim = dict()
-with open(os.path.abspath(sys.argv[2]), "r") as corpus_reader:
-    for i, line in enumerate(corpus_reader):
-        spl = line.strip().split(" ||| ")
+with open(os.path.abspath(sys.argv[2]), "r") as corpus_reader, open(os.path.abspath(sys.argv[3]), "r") as corpus_tok_reader:
+    for i, (line1, line2) in enumerate(zip(corpus_reader, corpus_tok_reader)):
+        sen_spl = line1.strip().split(" ||| ")
+        spl = line2.strip().split(" ||| ")
         sen_sim = sim(spl[0].lower(), spl[1].lower(), src2dst_dict)
         if sen_sim > 0.3:
-            if spl[0] not in src2dst_sen_max_sim:
-                src2dst_sen_max_sim[spl[0]] = (spl[1], sen_sim)
-            elif src2dst_sen_max_sim[spl[0]][1] < sen_sim:
-                src2dst_sen_max_sim[spl[0]] = (spl[1], sen_sim)
+            if sen_spl[0] not in src2dst_sen_max_sim:
+                src2dst_sen_max_sim[sen_spl[0]] = (sen_spl[1], sen_sim)
+            elif src2dst_sen_max_sim[sen_spl[0]][1] < sen_sim:
+                src2dst_sen_max_sim[sen_spl[0]] = (sen_spl[1], sen_sim)
 
             if spl[1] not in dst2src_sen_max_sim:
-                dst2src_sen_max_sim[spl[1]] = (spl[0], sen_sim)
-            elif dst2src_sen_max_sim[spl[1]][1] < sen_sim:
-                dst2src_sen_max_sim[spl[1]] = (spl[0], sen_sim)
+                dst2src_sen_max_sim[sen_spl[1]] = (sen_spl[0], sen_sim)
+            elif dst2src_sen_max_sim[sen_spl[1]][1] < sen_sim:
+                dst2src_sen_max_sim[sen_spl[1]] = (sen_spl[0], sen_sim)
         if i % 10000 == 0:
             print(i, end="\r")
 
 print("\nGetting best results")
 wrote = 0
-with open(os.path.abspath(sys.argv[3]), "w") as writer:
+with open(os.path.abspath(sys.argv[4]), "w") as writer:
     for i, src_sen in enumerate(src2dst_sen_max_sim.keys()):
         dst_sen, sen_sim = src2dst_sen_max_sim[src_sen]
         if dst2src_sen_max_sim[dst_sen][0] == src_sen:

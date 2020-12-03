e027668282d13fc8906851b5ae96268256c53297
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-12-03 13:02:12 -0500

    Fixed conditions for output layers

--- image_model.py
+++ image_model.py
@@ -310,7 +310,7 @@ class ImageCaptioning(Seq2Seq):
         batch_lang = int(src_langs[0])
 
         decoder = self.decoder if not self.lang_dec else self.decoder[batch_lang]
-        output_layer = self.output_layer if not self.lang_dec else self.output_layer[batch_lang]
+        output_layer = self.output_layer if (not self.lang_dec) and self.tie_embed else self.output_layer[batch_lang]
         tgt_langs = src_langs.unsqueeze(-1).expand(-1, tgt_inputs.size(-1)).to(device)
         if tgt_positions is not None:
             tgt_positions = tgt_positions[:, :-1].to(device)

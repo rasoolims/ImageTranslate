695fc30e1809ee3d4178523ea5ee2b911971a170
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-12-09 14:47:56 -0500

    Update create_caption_list_from_translation.py

--- scripts/create_caption_list_from_translation.py
+++ scripts/create_caption_list_from_translation.py
@@ -5,7 +5,7 @@ translations = dict()
 with open(os.path.abspath(sys.argv[1]), "r") as r:
     for line in r:
         spl = line.strip().split("|||")
-        translations[spl[0]] = spl[1]
+        translations[spl[0].strip()] = spl[1].strip()
 print("Extracted", len(translations), "unique translations!")
 
 with open(os.path.abspath(sys.argv[2]), "r") as r, open(os.path.abspath(sys.argv[3]), "w") as w:

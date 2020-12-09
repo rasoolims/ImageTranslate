c8a17dac84efc1a375eb46a022a39d9d4bc43447
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-12-09 14:46:03 -0500

    Create create_caption_list_from_translation.py

---
+++ scripts/create_caption_list_from_translation.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+translations = dict()
+with open(os.path.abspath(sys.argv[1]), "r") as r:
+    for line in r:
+        spl = line.strip().split("|||")
+        translations[spl[0]] = spl[1]
+print("Extracted", len(translations), "unique translations!")
+
+with open(os.path.abspath(sys.argv[2]), "r") as r, open(os.path.abspath(sys.argv[3]), "w") as w:
+    for line in r:
+        spl = line.strip().split("\t")
+        if spl[1] in translations:
+            w.write(spl[0] + "\t" + translations[spl[1]] + "\n")

2e12f460c77622977525711133bb9cc769546898
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2021-03-31 11:56:05 -0400

    Update soft_link_images_to_folder.py

--- scripts/flickr/soft_link_images_to_folder.py
+++ scripts/flickr/soft_link_images_to_folder.py
@@ -2,10 +2,13 @@ import os
 import sys
 
 output_folder = os.path.abspath(sys.argv[2])
+files = set()
 with open(os.path.abspath(sys.argv[1]), "r") as reader:
     for line in reader:
         spl = line.strip().split("\t")
         if len(spl) < 2: continue
 
-        command = "ln -s {0} {1}".format(spl[0], output_folder)
-        os.system(command)
+        if spl[0] not in files:
+            command = "ln -s {0} {1}".format(spl[0], output_folder)
+            os.system(command)
+            files.add(spl[0])
